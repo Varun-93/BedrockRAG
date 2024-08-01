f4ae7be497c5897ee088eb91193f45046727e012
Varun Bhatia <dstest12020@gmail.com>
2024-08-02 00:19:21 +0530

    'Added main function'

--- main.py
+++ main.py
@@ -2,3 +2,61 @@ import boto3
 import streamlit as st
 from langchain.llms.bedrock import Bedrock
 from langchain.embeddings import BedrockEmbeddings
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+
+prompt_template = """
+
+Human: Use the following pieces of context to provide a
+concise answer to the question at the end but atleadt summarize with 
+250 words with detailed explanations. If you don't know the answer ,
+just say that you don't know, don't try to make up any answer.
+<context>
+{context}
+</context>
+
+Question: {question}
+
+Assistant : """
+
+
+bedrock = boto3.client(service='bedrock-runtime', region_name = 'us-east-1')
+bedrock_embedding = BedrockEmbeddings(model_id='amazon.titan-embed-text-v1', client=bedrock)
+
+
+def getdocument():
+    loader = PyPDFDirectoryLoader("Data")
+    documents = loader.load()
+    test_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
+    docs = test_splitter.split_documents(documents)
+    return docs
+
+
+def get_vector_store(docs):
+    vectorstore_faiss = FAISS.from_documents(docs, bedrock_embedding)
+    vectorstore_faiss.save_local('faiss_local')
+
+
+def get_llm():
+    llm=Bedrock(model_id='mistral.mistral-7b-instruct-v0:2', client = bedrock)
+    return llm
+
+
+prompt = PromptTemplate(template=prompt_template, input_variables=['context','question'])
+
+def get_llm_response(llm, vectorstore_faiss, query):
+    qa = RetrievalQA.from_chain_type(llm=llm,
+                                     chain_type='stuff',
+                                     retriever=vectorstore_faiss.as_retriever(search_type='similarity', search_kwargs={'k':3}),
+                                     return_source_documents=True,
+                                     chain_type_kwargs={'prompt':prompt})
+    response = qa({'query':query})
+    return response['results']
+
+
+def main():
+    
